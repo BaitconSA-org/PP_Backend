2319b88289d2b1a26b064dbda23bd5159b1d4061
gustavomquintana <gustavo.quintanam@gmail.com>
2025-05-28 15:27:53 -0300

    Logica JS modificacion SupplierPortalService.js para interceptar expand PurchaseOrderExt V.1

--- srv/SupplierPortalService.js
+++ srv/SupplierPortalService.js
@@ -3,12 +3,23 @@ const cds = require('@sap/cds');
 module.exports = cds.service.impl(async function () {
   const s4 = await cds.connect.to('purchaseorder_edmx');
 
+  // Manejo manual de PurchaseOrderItemExt con soporte de $select, $top, $skip, etc.
+  this.on('READ', 'PurchaseOrderItemExt', async (req) => {
+    try {
+      console.log('>>> Handling custom READ for PurchaseOrderItemExt');
+      return await s4.run(req.query);  // delega con $select, $skip, etc. incluidos
+    } catch (err) {
+      console.error(err);
+      req.reject(500, 'Error delegating to remote service');
+    }
+  });
+
+  // Lógica de PurchaseOrderExt con expand manual (como ya tenés)
   this.on('READ', 'PurchaseOrderExt', async (req) => {
     let poHeaders;
 
     if (req.params && req.params.length) {
       const poNumber = req.params[0].PurchaseOrder;
-
       poHeaders = await s4.run(
         SELECT.from('PurchaseOrder').where({ PurchaseOrder: poNumber }),
       );
