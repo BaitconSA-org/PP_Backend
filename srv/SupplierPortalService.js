0de61fab068468931bf14bd3ed3d926c3eca8bae
Tomás Páez Fernández <tomaspf9@gmail.com>
2025-11-05 09:59:15 -0300

    borro error materialdoc ext

--- srv/SupplierPortalService.js
+++ srv/SupplierPortalService.js
@@ -765,18 +765,5 @@ module.exports = cds.service.impl(async function () {
       console.error('[ERROR] al leer MaterialDocumentExt:', error);
       return req.reject(500, 'Error al leer MaterialDocument desde S/4HANA');
     }
-  });
-  
-  this.on('READ', 'MaterialDocumentItemExt', async (req) => {
-    try {
-      const s4 = await cds.connect.to('A_MaterialDocument');
-      return await s4.run(req.query);
-    } catch (error) {
-      console.error('[ERROR] al leer MaterialDocumentItemExt:', error);
-      return req.reject(500, 'Error al leer MaterialDocumentItem desde S/4HANA');
-    }
-  });
-
-
-  
+  });  
 });
